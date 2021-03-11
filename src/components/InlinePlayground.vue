0ba71814a01059c0830ea524899139d1d68911a6
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-12 04:44:30 +0800

    chore: update

--- src/components/InlinePlayground.vue
+++ src/components/InlinePlayground.vue
@@ -1,21 +1,35 @@
 <script setup lang="ts">
-import { ref, computed, onMounted, watchEffect } from 'vue'
+/* eslint-disable vue/no-v-html */
+import { ref, computed, onMounted, watchEffect, defineProps } from 'vue'
+import { useThrottle } from '@vueuse/core'
 import Windi from 'windicss'
 import Prism from 'prismjs'
 
 import 'prismjs/components/prism-css'
 
+const props = defineProps({
+  classes: {
+    type: String,
+    // eslint-disable-next-line no-useless-escape
+    default: 'p-1.1em bg-hex-0ea5e9 text-white rounded\nhover:\(shadow bg-opacity-85)',
+  },
+  preview: {
+    default: true,
+  },
+})
+
 const processor = new Windi()
 
 const frame = ref<HTMLIFrameElement | null>()
-const source = ref('p-1em bg-hex-0ea5e9 text-white rounded\nhover\\:(shadow bg-opacity-85)'.replace(/\\/g, ''))
+const classes = ref(props.classes.replace(/::/g, ':'))
+const throttled = useThrottle(classes, 300)
 
 let acceped: string[] = []
 
 const style = computed(() => {
-  const { styleSheet, success } = processor.interpret(source.value.replace(/\n/g, ' '))
+  const { styleSheet, success } = processor.interpret(throttled.value.replace(/\n/g, ' '))
   acceped = success
-  return styleSheet.build()
+  return styleSheet.build().trim()
 })
 
 const highlighted = computed(() => {
@@ -42,20 +56,22 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="my-4 p-2 border border-gray-300 rounded-full">
+  <div class="my-4 p-2 border border-gray-300 rounded">
     <textarea
-      v-model="source"
+      v-model="classes"
       class="w-full border border-gray-300 resize-y p-2 rounded"
     />
-    <div class="grid grid-cols-2 mt-2">
-      <div class="language-css text-white !m-0">
+    <div class="flex">
+      <div class="language-css !m-0">
         <pre v-html="highlighted" />
       </div>
-      <iframe
-        ref="frame"
-        src="/__playground.html"
-        class="border-none overflow-hidden outline-none pl-2 w-full"
-      />
+      <div class="w-2/5 hidden md:block pl-2">
+        <iframe
+          ref="frame"
+          src="/__playground.html"
+          class="border-none overflow-hidden outline-none w-full"
+        />
+      </div>
     </div>
   </div>
 </template>
