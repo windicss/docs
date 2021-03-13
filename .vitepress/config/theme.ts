7d89eb0bdec6e8c82447c4f19f3852da39daa3c9
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-13 20:17:09 +0800

    feat: utilities dropdown

--- .vitepress/config/theme.ts
+++ .vitepress/config/theme.ts
@@ -16,8 +16,29 @@ const themeConfig: DefaultTheme.Config = {
 
   nav: [
     { text: 'Guide', link: '/guide/' },
-    { text: 'Utilities', link: '/utilities/' },
-    // { text: 'Tailwind CSS', link: 'https://tailwindcss.com/docs' },
+    {
+      text: 'Utilities',
+      items: [
+        { text: 'Accessibility', link: '/utilities/accessibility' },
+        { text: 'Backgrounds', link: '/utilities/backgrounds' },
+        { text: 'Borders', link: '/utilities/borders' },
+        { text: 'Box Alignment', link: '/utilities/box-alignment' },
+        { text: 'Colors', link: '/utilities/colors' },
+        { text: 'Effects', link: '/utilities/effects' },
+        { text: 'Flexbox', link: '/utilities/flexbox' },
+        { text: 'Grid', link: '/utilities/grid' },
+        { text: 'Interactivity', link: '/utilities/interactivity' },
+        { text: 'Layout', link: '/utilities/layout' },
+        { text: 'Sizing', link: '/utilities/sizing' },
+        { text: 'Spacing', link: '/utilities/spacing' },
+        { text: 'SVG', link: '/utilities/svg' },
+        { text: 'Tables', link: '/utilities/tables' },
+        { text: 'Transforms', link: '/utilities/transforms' },
+        { text: 'Transitions', link: '/utilities/transitions' },
+        { text: 'Typography', link: '/utilities/typography' },
+        { text: 'Variants', link: '/utilities/variants' },
+      ],
+    },
     {
       text: 'Community',
       items: [
