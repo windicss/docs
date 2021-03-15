f16aac00a8107fb42b6a5e25e75049e514e701d1
hanlee <me@hanlee.co>
2021-03-15 20:20:47 +0900

    fix: allow quote-props in md file

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/no-var-requires': 'off',
+        'quote-props': 'off',
       },
     },
   ],
