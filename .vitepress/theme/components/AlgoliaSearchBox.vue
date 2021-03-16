1d76ce943a1c5a358ff2db2252d5662b4ee8ebc4
hanlee <me@hanlee.co>
2021-03-17 04:35:24 +0900

    feat: new search model style

--- .vitepress/theme/components/AlgoliaSearchBox.vue
+++ .vitepress/theme/components/AlgoliaSearchBox.vue
@@ -138,14 +138,14 @@ function initialize(userOptions: any) {
   --docsearch-modal-background: var(--c-bg);
   --docsearch-modal-shadow: var(--c-bg);
   --docsearch-hit-color: var(--c-text-light);
-  --docsearch-footer-background: rgba(125,125,125,0.1);
+  --docsearch-footer-background: transparent;
   --docsearch-footer-shadow: rgba(125,125,125,0.1);
   --docsearch-hit-background: rgba(125,125,125,0.1);
   --docsearch-hit-shadow: none;
   --docsearch-primary-color: var(--c-brand);
   --docsearch-highlight-color: var(--docsearch-primary-color);
   --docsearch-searchbox-background: theme('colors.gray.200');
-  --docsearch-searchbox-focus-background: theme('colors.gray.200');
+  --docsearch-searchbox-focus-background: theme('colors.gray.100');
   --docsearch-searchbox-shadow: none;
   --docsearch-text-color: var(--c-text-light);
   --docsearch-muted-color: var(--c-text-lighter);
@@ -155,14 +155,14 @@ function initialize(userOptions: any) {
 
 html.dark .DocSearch {
   --docsearch-container-background: rgba(0,0,0,0.8);
-  --docsearch-searchbox-background: theme('colors.gray.200');
-  --docsearch-searchbox-focus-background: theme('colors.gray.200');
+  --docsearch-searchbox-background: rgba(255, 255, 255, 0.5);
+  --docsearch-searchbox-focus-background: rgba(255, 255, 255, 0.1);
   --docsearch-key-gradient: transparent;
   --docsearch-key-shadow: none;
 }
 
 .DocSearch-Button {
-  @apply rounded-md px-3 ml-0 rounded-lg
+  @apply rounded-lg px-3 ml-0 rounded-lg
   bg-gray-200 bg-opacity-50 hover:bg-opacity-60 hover:bg-gray-200
   dark:(bg-gray-700 bg-opacity-40 hover:bg-opacity-60);
 }
@@ -179,7 +179,15 @@ html.dark .DocSearch {
 .DocSearch-Button:hover .DocSearch-Button-Key {
   @apply text-$docsearch-text-color border-$docsearch-text-color;
 }
-
+.DocSearch-Form, .DocSearch-Hit a {
+  @apply rounded-lg;
+}
+.DocSearch-Modal {
+  @apply rounded-lg;
+}
+.DocSearch-Footer {
+  @apply border-t border-dashed dark:border-gray-500;
+}
 .DocSearch-Search-Icon {
   @apply h-18px w-18px md:(h-16px w-16px);
 }
