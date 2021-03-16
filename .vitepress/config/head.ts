03e53397a8e62c4fda44acd7f0bff4686680e96e
hanlee <me@hanlee.co>
2021-03-17 00:33:11 +0900

    feat: make fonts load faster

--- .vitepress/config/head.ts
+++ .vitepress/config/head.ts
@@ -24,6 +24,8 @@ const head: HeadConfig[] = [
   ['meta', { property: 'og:image', content: metaData.image }],
   ['meta', { property: 'og:description', content: metaData.description }],
 
+  ['link', { rel: 'dns-prefetch', href: 'https://fonts.gstatic.com' }],
+  ['link', { rel: 'preconnect', crossorigin: 'anonymous', href: 'https://fonts.gstatic.com' }],
   ['link', { href: 'https://fonts.googleapis.com/css2?family=Fira+Code&family=Inter:wght@200;400;500;600&display=swap', rel: 'stylesheet' }],
 ]
 
