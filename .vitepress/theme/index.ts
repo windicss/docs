030693db4bd3b9a47c45463ab12683cceb85e6a9
hanlee <me@hanlee.co>
2021-03-18 01:30:19 +0900

    feat: add sidebar link active background

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,7 +4,7 @@ import './styles/vars.css'
 import './styles/layout.postcss'
 import './styles/code.css'
 import './styles/custom-blocks.css'
-import './styles/sidebar-links.css'
+import './styles/sidebar-links.postcss'
 
 import { Theme } from 'vitepress'
 import Layout from './Layout.vue'
