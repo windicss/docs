cf4055318d87f5a3ac74d98ce32519d29ecfc6e1
hanlee <me@hanlee.co>
2021-04-05 20:28:09 +0900

    fix: vue import

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig({
       'json5',
       'lz-string',
       '@vueuse/core',
+      '@vueuse/integrations',
       'windicss',
       'windicss/colors',
       'windicss/utils/style',
