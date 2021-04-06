0d056286e131055c0499054a293d33837bb09fc7
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-06 12:34:40 +0800

    docs: update og image

--- .vitepress/config/constants.ts
+++ .vitepress/config/constants.ts
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production'
 const site = isProd ? 'https://windicss.netlify.app' : 'http://localhost:3000'
 
 export const metaData = {
-  title: 'Windi CSS 🌬',
+  title: 'Windi CSS',
   description: 'Next generation utility-first CSS framework.',
   site,
-  image: `${site}/assets/banner.png`,
+  image: `${site}/assets/og-image.png`,
 }
