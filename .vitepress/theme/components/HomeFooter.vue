e4389454440b9773bf55135d8409cea70b5f040b
hanlee <me@hanlee.co>
2021-05-12 02:26:44 +0900

    fix: class name overwrite

--- .vitepress/theme/components/HomeFooter.vue
+++ .vitepress/theme/components/HomeFooter.vue
@@ -1,21 +1,9 @@
 <template>
   <footer v-if="$frontmatter.footer" class="mx-auto max-w-960px md:px-6">
-    <div class="container">
+    <div class="pt-8 px-6 pb-9 xs:(pt-12 px-6 pb-13)">
       <p class="m-0 text-center leading-6 text-0.9rem text-$c-text-light">
         {{ $frontmatter.footer }}
       </p>
     </div>
   </footer>
 </template>
-
-<style scoped lang="postcss">
-.container {
-  padding: 2rem 1.5rem 2.25rem;
-}
-
-@screen xs {
-  .container {
-    padding: 3rem 1.5rem 3.25rem;
-  }
-}
-</style>
