1434d7bab590fb819ad012b25de890d0ab9e6134
hanlee <me@hanlee.co>
2021-05-12 02:26:53 +0900

    chore: cleanup

--- .vitepress/theme/components/Home.vue
+++ .vitepress/theme/components/Home.vue
@@ -4,7 +4,7 @@
     <slot name="hero" />
     <!-- <HomeFeatures /> -->
 
-    <div class="home-content">
+    <div class="max-w-62rem mx-auto py-6 md:p-0">
       <Content />
     </div>
     <slot name="features" />
@@ -12,29 +12,3 @@
     <slot name="footer" />
   </main>
 </template>
-
-<script setup lang="ts">
-// import { onMounted, onUnmounted } from 'vue'
-// let emitted = false
-// function scrollDown() {
-//   if (!emitted) {
-//     window.scrollTo({ top: 10000, behavior: 'smooth' })
-//     emitted = true
-//   }
-//   if (window.scrollY === 0)
-//     emitted = false
-// }
-// onMounted(() => {
-//   document.addEventListener('scroll', scrollDown)
-// })
-
-// onUnmounted(() => {
-//   document.removeEventListener('scroll', scrollDown)
-// })
-</script>
-
-<style scoped lang="postcss">
-.home-content {
-  @apply max-w-62rem mx-auto py-6 md:(p-0);
-}
-</style>
