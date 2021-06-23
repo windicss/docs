aac23c275ea68ac0039770a99e53ee307fe0afa6
hanlee <me@hanlee.co>
2021-06-24 00:24:31 +0900

    fix: not found layout in mobile

--- .vitepress/theme/NotFound.vue
+++ .vitepress/theme/NotFound.vue
@@ -6,11 +6,11 @@ defineProps({})
 </script>
 
 <template>
-  <div class="relative h-60">
-    <h1 class="!m-0 absolute top-20 w-full text-center !text-12xl !text-transparent text-stroke-sm text-stroke-primary-300 dark:text-stroke-primary-700">
+  <div class="relative flex flex-col items-center justify-center h-60vh">
+    <h1 class="!m-0 w-full text-center !text-9xl !md:text-12xl !text-transparent text-stroke-sm text-stroke-primary-300 dark:text-stroke-primary-700">
       404
     </h1>
-    <h2 class="!m-0 absolute top-45 w-full text-center !text-3xl text-stroke-sm uppercase tracking-0.5em pl-3">
+    <h2 class="!m-0 !-mt-15 !md:-mt-20 w-full text-center !text-3xl text-stroke-sm uppercase tracking-0.5em pl-3">
       Page Not Found
     </h2>
   </div>
