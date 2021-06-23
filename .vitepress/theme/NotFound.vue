160de1402b8c52686bdcfb708c6d9573baa79e2e
hanlee <me@hanlee.co>
2021-06-24 00:12:30 +0900

    feat: add not found page

---
+++ .vitepress/theme/NotFound.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps({})
+
+</script>
+
+<template>
+  <div class="relative h-60">
+    <h1 class="!m-0 absolute top-20 w-full text-center !text-12xl !text-transparent text-stroke-sm text-stroke-primary-300 dark:text-stroke-primary-700">
+      404
+    </h1>
+    <h2 class="!m-0 absolute top-45 w-full text-center !text-3xl text-stroke-sm uppercase tracking-0.5em pl-3">
+      Page Not Found
+    </h2>
+  </div>
+</template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -8,10 +8,11 @@ import './style/markdown.css'
 import './style/codemirror-prism-vars.css'
 
 import Layout from './Layout.vue'
+import NotFound from './NotFound.vue'
 
 export default <Theme> {
   Layout,
-  NotFound: () => 'custom 404', // <- this is a Vue 3 functional component
+  NotFound, // <- this is a Vue 3 functional component
   enhanceApp({ app, router, siteData }) {
     // app is the Vue 3 app instance from `createApp()`. router is VitePress'
     // custom router. `siteData`` is a `ref`` of current site-level metadata.
