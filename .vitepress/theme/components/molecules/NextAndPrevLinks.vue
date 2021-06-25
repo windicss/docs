75286713681a6d5f05e3569317fe6feeffd9352f
hanlee <me@hanlee.co>
2021-06-25 15:42:21 +0900

    fix: next and prev links layout in mobile size

--- .vitepress/theme/components/molecules/NextAndPrevLinks.vue
+++ .vitepress/theme/components/molecules/NextAndPrevLinks.vue
@@ -1,18 +1,14 @@
 <template>
   <div v-if="hasLinks">
-    <div class="flex border-t border-$windi-bc pt-6 justify-between">
-      <div class="max-w-1/2">
-        <a v-if="prev" class="nav-link" :href="withBase(prev.link)">
-          <carbon-arrow-left class="mr-3" />
-          <span>{{ prev.text }}</span>
-        </a>
-      </div>
-      <div class="max-w-1/2 text-right">
-        <a v-if="next" class="nav-link" :href="withBase(next.link)">
-          <span>{{ next.text }}</span>
-          <carbon-arrow-right class="ml-3" />
-        </a>
-      </div>
+    <div class="flex border-t border-$windi-bc pt-6 justify-between items-center">
+      <a v-if="prev" class="nav-link" :href="withBase(prev.link)">
+        <carbon-arrow-left class="mr-3" />
+        <span class="text-sm md:text-base">{{ prev.text }}</span>
+      </a>
+      <a v-if="next" class="nav-link text-right justify-end" :href="withBase(next.link)">
+        <span class="text-sm md:text-base">{{ next.text }}</span>
+        <carbon-arrow-right class="ml-3" />
+      </a>
     </div>
   </div>
 </template>
@@ -27,8 +23,9 @@ const { hasLinks, prev, next } = useNextAndPrevLinks()
 <style scoped lang="postcss">
 .nav-link {
   @apply
+    flex-1
     inline-flex items-center px-3
-    font-semibold text-sm md:text-base
+    font-semibold
     text-cool-gray-500 dark:text-gray-400
     hover:(text-gray-800 dark:text-white);
 }
