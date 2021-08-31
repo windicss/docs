c783cabc014d4d420db0a601518ca63ea66534c1
hanlee <me@hanlee.co>
2021-08-31 16:52:47 +0900

    chore: wrap news text in mobile size

--- .vitepress/theme/components/molecules/HomeNews.vue
+++ .vitepress/theme/components/molecules/HomeNews.vue
@@ -14,7 +14,7 @@ const { frontmatter } = useData()
         inline-flex rounded
         font-bold text-center text-primary
         p-3 md:p-4 lg:px-8
-        text-sm lg:text-base"
+        text-sm lg:text-base mx-8"
       :href="frontmatter.newsLink"
     >
       {{ frontmatter.newsTitle }}
